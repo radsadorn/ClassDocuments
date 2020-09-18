36ca6757db0fede3816dbf00283042fea6c93623
radsadorn <sutthiratphutho@gmail.com>
2020-09-18 15:58:32 +0700

    Week 7

---
+++ 2D - 1/DATA STRUCTURES AND ALGORITHM/Week 7 Recursion/02 Length of a String EXTRA.py
@@ -0,0 +1,12 @@
+def length(txt):
+    if txt[0:] is txt[-1:]:
+        print(txt[0], end = "*")
+        return 1
+
+    s = length(txt[:-1])
+    print(txt[s], end = "*" if s % 2 == 0 else "~")
+    return s + 1
+
+
+if __name__ == '__main__':
+    print("\n",length(input("Enter Input : ")),sep="")
---
+++ 2D - 1/DATA STRUCTURES AND ALGORITHM/Week 7 Recursion/05 Asteroid.py
@@ -0,0 +1,22 @@
+def asteroid_collision(asts):
+    if len(asts) <= 1:
+        return asts
+
+    temp = asteroid_collision(asts[1:])
+
+    if len(temp) != 0 and asts[0] >= 0 and temp[0] <= 0:
+        if asts[0] > abs(temp[0]):
+            return asteroid_collision([asts[0]] + temp[1:])
+        elif asts[0] == 0 and temp[0] == 0:
+            return [asts[0]] + temp
+        elif asts[0] == abs(temp[0]):
+            return temp[1:] if len(temp) > 1 else []
+        else:
+            return temp
+    else:
+        return [asts[0]] + temp
+
+if __name__ == '__main__':
+    x = input("Enter Input : ").split(",")
+    x = list(map(int,x))
+    print(asteroid_collision(x))
