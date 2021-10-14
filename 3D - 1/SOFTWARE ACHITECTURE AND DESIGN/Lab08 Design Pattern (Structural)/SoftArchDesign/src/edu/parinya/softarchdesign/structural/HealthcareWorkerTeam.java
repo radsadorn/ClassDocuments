2b66abf7f8810ebd48573d420004f43dc5037ea9
SUTTHIRAT PHUTHO <61104175+radsadorn@users.noreply.github.com>
2021-10-14 18:03:35 +0700

    Update HealthcareWorkerTeam.java

--- 3D - 1/SOFTWARE ACHITECTURE AND DESIGN/Lab08 Design Pattern (Structural)/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
+++ 3D - 1/SOFTWARE ACHITECTURE AND DESIGN/Lab08 Design Pattern (Structural)/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
@@ -1,2 +1,31 @@
-package edu.parinya.softarchdesign.structural;public class HealthcareWorkerTeam {
+package edu.parinya.softarchdesign.structural;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+    private Set<HealthcareServiceable> members = new LinkedHashSet<>();
+
+    public void addMember(HealthcareServiceable member) {
+        this.members.add(member);
+    }
+
+    public void removeMember(HealthcareServiceable member) {
+        this.members.remove(member);
+    }
+
+    @Override
+    public void service() {
+        for(HealthcareServiceable member: this.members)
+            member.service();
+    }
+
+    @Override
+    public double getPrice() {
+        double price = 0;
+        for(HealthcareServiceable member: this.members)
+            price += member.getPrice();
+        return price;
+    }
 }
