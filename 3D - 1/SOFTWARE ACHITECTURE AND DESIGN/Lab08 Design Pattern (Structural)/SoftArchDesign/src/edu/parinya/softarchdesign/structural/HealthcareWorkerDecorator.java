5a9afd9d4aee647c9e8293fe18616e03c876e4d6
SUTTHIRAT PHUTHO <61104175+radsadorn@users.noreply.github.com>
2021-10-14 18:02:47 +0700

    Update HealthcareWorkerDecorator.java

--- 3D - 1/SOFTWARE ACHITECTURE AND DESIGN/Lab08 Design Pattern (Structural)/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerDecorator.java
+++ 3D - 1/SOFTWARE ACHITECTURE AND DESIGN/Lab08 Design Pattern (Structural)/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerDecorator.java
@@ -1,2 +1,19 @@
-package edu.parinya.softarchdesign.structural;public class HealthcareWorkerDecorator {
+package edu.parinya.softarchdesign.structural;
+
+public abstract class HealthcareWorkerDecorator extends HealthcareWorker{
+    protected HealthcareServiceable worker;
+
+    HealthcareWorkerDecorator(HealthcareServiceable worker) {
+        super((HealthcareWorker) worker);
+        this.worker = worker;
+        System.out.print("Decorate " + this.getName());
+    }
+
+    public void service() {
+        this.worker.service();
+    }
+
+    public double getPrice() {
+        return this.worker.getPrice();
+    }
 }
