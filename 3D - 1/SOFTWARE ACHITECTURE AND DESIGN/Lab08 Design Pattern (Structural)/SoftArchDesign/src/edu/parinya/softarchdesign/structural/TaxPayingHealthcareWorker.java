49c35878eb66419cd7ebb5e4f6aab8529cc0a059
SUTTHIRAT PHUTHO <61104175+radsadorn@users.noreply.github.com>
2021-10-14 18:03:58 +0700

    Update TaxPayingHealthcareWorker.java

--- 3D - 1/SOFTWARE ACHITECTURE AND DESIGN/Lab08 Design Pattern (Structural)/SoftArchDesign/src/edu/parinya/softarchdesign/structural/TaxPayingHealthcareWorker.java
+++ 3D - 1/SOFTWARE ACHITECTURE AND DESIGN/Lab08 Design Pattern (Structural)/SoftArchDesign/src/edu/parinya/softarchdesign/structural/TaxPayingHealthcareWorker.java
@@ -1,2 +1,14 @@
-package edu.parinya.softarchdesign.structural;public class TaxPayingHealthcareWorker {
+package edu.parinya.softarchdesign.structural;
+
+public class TaxPayingHealthcareWorker extends HealthcareWorkerDecorator{
+    TaxPayingHealthcareWorker(HealthcareServiceable worker) {
+        super(worker);
+        System.out.println(" with TaxPaying");
+    }
+
+    public String getName() { return super.getName(); }
+
+    public double getPrice() {
+        return super.getPrice() * 1.10;
+    }
 }
